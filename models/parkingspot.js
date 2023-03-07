c2386636b03e8fd30f288f8d1c144daf08ac34dd
saidov-arb <arbi.saidov@htl-wels.at>
2023-03-07 17:34:52 +0100

    model - Changed model of parkingspot and edited some comments

--- models/parkingspot.js
+++ models/parkingspot.js
@@ -4,6 +4,20 @@ const addressSchema = require('./address.js')
 const reservationSchema = require('./reservation.js')
 const timeframeSchema = require('./timeframe.js')
 
+/** 
+ * p_status:
+ *    possible values: 'active', 'inactive', 'deleted'
+ *    'active' 
+ *         parkingspot is available for reservations
+ *    'inactive' 
+ *         parkingspot is not available for more reservations
+ *         possible reservations will be canceled
+ *    'deleted' 
+ *         parkingspot is not available for reservations
+ *         possible reservations will be canceled
+ *         will be deleted after the last reservation what coudn't be canceled is over
+*/
+
 const parkingspotSchema = new mongoose.Schema({
     p_owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     p_number: {
@@ -18,10 +32,10 @@ const parkingspotSchema = new mongoose.Schema({
         type: String,
         required: false
     }],
-    p_deleteflag: {
-        type: Boolean,
+    p_status: {
+        type: String,
         required: false,
-        default: false,
+        default: 'active',
     },
     pt_availability: timeframeSchema,
     pr_reservations: [reservationSchema],
